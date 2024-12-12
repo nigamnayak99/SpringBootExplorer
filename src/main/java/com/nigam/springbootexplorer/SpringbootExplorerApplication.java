a6e0d8f9da97a8cf6955e215ae33552d53aeb8e4
nigamnayak99 <nigamnayak9@gmail.com>
2024-12-12 23:59:00 +0530

    Initialze Commit

---
+++ src/main/java/com/nigam/springbootexplorer/SpringbootExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.nigam.springbootexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nigam/springbootexplorer/SpringbootExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nigam.springbootexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
