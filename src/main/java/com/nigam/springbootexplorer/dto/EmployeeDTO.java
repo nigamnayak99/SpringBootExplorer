b8eb6deda3fe4bb372f879cdf0b8a3e320d36d76
nigamnayak99 <nigamnayak9@gmail.com>
2024-12-16 23:33:10 +0530

    Update Validation for EmployeeDTO

--- src/main/java/com/nigam/springbootexplorer/dto/EmployeeDTO.java
+++ src/main/java/com/nigam/springbootexplorer/dto/EmployeeDTO.java
@@ -3,7 +3,7 @@ package com.nigam.springbootexplorer.dto;
 
 import com.nigam.springbootexplorer.annotations.JobTitleValidation;
 import com.nigam.springbootexplorer.entity.Employee;
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -15,20 +15,45 @@ import java.time.LocalDateTime;
 @Builder
 public class EmployeeDTO {
 
+    @NotBlank(message = "This is a required field: employeeId")
     private Integer employeeId;
+
+    @NotBlank(message = "This is a required field: firstName")
     private String firstName;
+
+    @NotBlank(message = "This is a required field: lastName")
     private String lastName;
+
     @Email
     private String email;
+
+    @NotBlank(message = "This is a required field: phoneNumber")
     private String phoneNumber;
+
+    @NotBlank
+    @FutureOrPresent
+    @PastOrPresent
     private LocalDate hireDate;
+
     @JobTitleValidation
     private String jobTitle;
+
+    @Positive
+    @NotBlank(message = "Salary Can't be Zero")
+    @Max(value = 1000000)
     private BigDecimal salary;
+
+    @Positive
+    @NotBlank(message = "Department Can't be Null")
     private Integer departmentId;
+
     private Integer managerId;
-    private LocalDateTime createdAt;   // Added createdAt field
-    private LocalDateTime updatedAt;   // Added updatedAt field
+
+    @PastOrPresent
+    private LocalDateTime createdAt;
+
+    @PastOrPresent
+    private LocalDateTime updatedAt;
 
     public static Employee toEntity(EmployeeDTO employeeDTO) {
         return Employee.builder()
