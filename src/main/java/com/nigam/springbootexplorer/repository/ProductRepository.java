3db21bee9507aa2df563b898a302754b06c081d7
nigamnayak99 <nigamnayak9@gmail.com>
2025-01-07 23:30:07 +0530

    add eample for structured jpa quries.'

--- src/main/java/com/nigam/springbootexplorer/repository/ProductRepository.java
+++ src/main/java/com/nigam/springbootexplorer/repository/ProductRepository.java
@@ -4,8 +4,17 @@ import com.nigam.springbootexplorer.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findBy();
+    List<Product> findByTitleContaining(String keyword);
+    List<Product> findByQuantityGreaterThan(Integer Quantity);
+    List<Product> findPriceLessThanAndQuantityGreaterThan(BigDecimal price, Integer quantity);
+    List<Product> findAllByOrderByPriceAsc();
+    List<Product> findAllByOrderByQuantityDesc();
+    List<Product> findTop5ByOrderByPriceDesc();
 
 }
