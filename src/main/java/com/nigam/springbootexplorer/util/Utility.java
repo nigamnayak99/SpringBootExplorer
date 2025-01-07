c2749de52a221893951d29d0afd3c26628fc02a1
nigamnayak99 <nigamnayak9@gmail.com>
2025-01-07 22:15:56 +0530

    add method to create a blank Rest Response

--- src/main/java/com/nigam/springbootexplorer/util/Utility.java
+++ src/main/java/com/nigam/springbootexplorer/util/Utility.java
@@ -1,5 +1,13 @@
 package com.nigam.springbootexplorer.util;
 
+import com.nigam.springbootexplorer.rest.RestResponse;
+
 public class Utility {
+
+    public static RestResponse initializeRestResponse(String serviceReferenceId) {
+        RestResponse restResponse = new RestResponse();
+        restResponse.getMetaData().setServiceReferenceId(serviceReferenceId);
+        return restResponse;
+    }
 
 }
