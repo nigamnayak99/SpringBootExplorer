a1733f31696e8b990576ec297259f0238b9666a9
nigamnayak99 <nigamnayak9@gmail.com>
2025-01-25 01:13:13 +0530

    Add SecurityFilterChain -> Create a SecurityFilterChain by exposing a bean of it self. -> It also exposes PasswordEncoder and AuthenticationManager.

--- src/main/java/com/nigam/springbootexplorer/configurations/WebSecurityConfig.java
+++ src/main/java/com/nigam/springbootexplorer/configurations/WebSecurityConfig.java
@@ -3,22 +3,22 @@ package com.nigam.springbootexplorer.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Configuration
 @EnableWebSecurity
@@ -27,33 +27,43 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/products").permitAll()
-                        .requestMatchers("/employee/**").hasAnyRole("ADMIN")
+        httpSecurity.sessionManagement(sessionConfigPolicy ->
+                        sessionConfigPolicy.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                    .csrf(AbstractHttpConfigurer::disable);   /* Disable CSRF Config */
+                     /* Disable Default Form Login */
+//                .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
 
-    @Bean
-    UserDetailsService inMemoryUserDetailsService() {
-        UserDetails inMemoryUser1 = User.withUsername("Anu")
-                .password(encode().encode("pass1")) // Use encode() here
-                .roles("USER")
-                .build();
 
-        UserDetails inMemoryAdminUser1 = User.withUsername("Nigam2")
-                .password(encode().encode("pass2")) // Use encode() here
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(inMemoryUser1, inMemoryAdminUser1);
-    }
+    /* Disable InMemoryUserDetail */
+//    @Bean
+//    UserDetailsService inMemoryUserDetailsService() {
+//        UserDetails inMemoryUser1 = User.withUsername("Anu")
+//                .password(encode().encode("pass1")) // Use encode() here
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails inMemoryAdminUser1 = User.withUsername("Nigam2")
+//                .password(encode().encode("pass2")) // Use encode() here
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(inMemoryUser1, inMemoryAdminUser1);
+//    }
 
     @Bean
     PasswordEncoder encode() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
