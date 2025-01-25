ef7df3e6fd55a0ed36f7828b3ab9b2fa1e912c62
nigamnayak99 <nigamnayak9@gmail.com>
2025-01-25 22:43:51 +0530

    Rename ModelMapperConfig to AppConfig to avoid circular Dependecny,

--- src/main/java/com/nigam/springbootexplorer/configurations/AppConfig.java
+++ src/main/java/com/nigam/springbootexplorer/configurations/AppConfig.java
@@ -1,15 +1,22 @@
 package com.nigam.springbootexplorer.configurations;
 
-
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class ModelMapperConfig {
+public class AppConfig {
 
     @Bean
     public ModelMapper getModelMapperBean() {
         return new ModelMapper();
     }
+
+    @Bean
+    PasswordEncoder encode() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
