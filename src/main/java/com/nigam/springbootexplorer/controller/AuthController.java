515b7bc1e41b11dbfa4d6c48ce0692a97d773a98
nigamnayak99 <nigamnayak9@gmail.com>
2025-01-26 21:49:02 +0530

    Introduce a new controller to refresh token.

--- src/main/java/com/nigam/springbootexplorer/controller/AuthController.java
+++ src/main/java/com/nigam/springbootexplorer/controller/AuthController.java
@@ -1,18 +1,24 @@
 package com.nigam.springbootexplorer.controller;
 
-import com.nigam.springbootexplorer.dto.LogInDTO;
+import com.nigam.springbootexplorer.dto.LogInRequestDTO;
+import com.nigam.springbootexplorer.dto.LogInResponseDTO;
 import com.nigam.springbootexplorer.dto.SignUpDTO;
 import com.nigam.springbootexplorer.dto.UserDTO;
 import com.nigam.springbootexplorer.rest.RestResponse;
 import com.nigam.springbootexplorer.services.UserDetailService;
 import com.nigam.springbootexplorer.services.UserLogInService;
 import com.nigam.springbootexplorer.util.Utility;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @Slf4j
 @RestController
@@ -35,12 +41,27 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public RestResponse login(@RequestBody LogInDTO logInDTO) {
+    public RestResponse login(@RequestBody LogInRequestDTO logInRequestDTO, HttpServletRequest request, HttpServletResponse response) {
         log.debug("---------login: Begin----");
         RestResponse restResponse = Utility.initializeRestResponse(null);
-        UserDTO userDTO = userLogInService.login(logInDTO);
-        restResponse.setResponseData(userDTO);
+        LogInResponseDTO logInResponseDTO = userLogInService.login(logInRequestDTO, response);
+        restResponse.setResponseData(logInResponseDTO);
         log.debug("---------login: End------");
+        return restResponse;
+    }
+
+    @PostMapping("/refresh")
+    public RestResponse refresh(HttpServletRequest request) {
+        log.debug("---------refresh: Begin----");
+        RestResponse restResponse = Utility.initializeRestResponse(null);
+        String refreshToken =  Arrays.stream(request.getCookies())
+                                    .filter(cookie -> "refreshToken".equals(cookie.getName()))
+                                    .findFirst()
+                                    .map(cookie -> cookie.getValue())
+                                    .orElseThrow(()-> new RuntimeException("Refresh Token Could not be found"));
+        LogInResponseDTO logInResponseDTO = userLogInService.refreshToken(refreshToken);
+        restResponse.setResponseData(logInResponseDTO);
+        log.debug("---------refresh: End------");
         return restResponse;
     }
 }
